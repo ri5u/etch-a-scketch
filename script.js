27d914d9695f4264b8b4048261ccd4f4159f01b1
r15u <rk5718158@gmail.com>
2025-06-16 18:40:08 +0530

    grids

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //Creating the square divs.
+const container = document.querySelector(".container");
+for(let i = 0; i < 16; i++){
+    const box = document.createElement("div");
+    box.classList.add("box");
+    container.appendChild(box);
+}
